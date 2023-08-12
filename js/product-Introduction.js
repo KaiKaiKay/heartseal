3a586e00de9ed2aef2d1a9c451d5e8f8d4c91f1f
kay <candy168741@gmail.com>
2023-08-12 18:53:44 +0800

    all

---
+++ js/product-Introduction.js
@@ -0,0 +1,91 @@
+//這邊是加入最愛
+function switchFavorite() {
+    let heart = document.getElementById("heart");
+    if (heart.title === "加入最愛") {
+        heart.src = "../../../pic/products_inner/heart-red.png";
+        heart.title = "取消最愛";
+    }
+    else {
+        heart.src = "../../../pic/products_inner/heart.png";
+        heart.title = "加入最愛";
+    }
+}
+
+function initFavorite() {
+    document.getElementById("heart").onclick = switchFavorite;
+}
+window.addEventListener("load", init, false);
+
+// 這邊是手機輪播
+$(document).ready(function () {
+    let divWidth = $('#sliderBoard').width();
+    let imgCount = $('#content li').length;
+    for (let i = 0; i < imgCount; i++) {
+        $('#contentButton').append('<li></li>');
+    }
+    $('#contentButton li:first').addClass('clicked');
+
+    $('#content li').width(divWidth);
+    $('#content').width(divWidth * imgCount);
+
+    let index = 0;
+    let timer = setInterval(moveToNext, 3000);
+
+    $('#contentButton li').click(function () {
+        clearInterval(timer);
+        index = $(this).index();
+
+        $('#content').css({
+            'transform': `translateX(${-index * divWidth}px)`
+        });
+
+        $(this).addClass('clicked');
+        $('#contentButton li').not(this).removeClass('clicked');
+
+        timer = setInterval(moveToNext, 3000);
+    });
+
+    function moveToNext() {
+        if (index < imgCount - 1) {
+            index++;
+        } else {
+            index = 0;
+        }
+
+        $('#content').css({
+            'transform': `translateX(${-index * divWidth}px)`
+        });
+
+        $(`#contentButton li:eq(${index})`).addClass('clicked');
+        $(`#contentButton li`).not(`:eq(${index})`).removeClass('clicked');
+    }
+
+    $(window).resize(function () {
+        divWidth = $('#sliderBoard').width();
+        $('#content li').width(divWidth);
+        $('#content').width(divWidth * imgCount);
+        $('#content').css({
+            'transform': `translateX(${-index * divWidth}px)`
+        });
+    });
+});
+
+//這邊是大圖換小圖
+    function showLarge(e) {
+        document.getElementById("large").src = e.target.src;
+    }
+
+    function init() {
+        let smalls = document.getElementsByClassName("small");
+        for (let i = 0; i < smalls.length; i++) {
+            smalls[i].onclick = showLarge;
+        }
+    }
+    window.addEventListener("load", function () {
+        initFavorite();
+        initShowLarge();
+    }, false);
+
+
+
+
--- js/same.js
+++ js/same.js
@@ -1,13 +1,26 @@
-const userDiv = document.querySelector(".user");
+//這邊是漢堡
+function toggleNav() {
+    const navArea = document.querySelector('.nav-area');
+    navArea.classList.toggle('open');
+}
+// 這邊是login
+const userDiv = document.querySelector("#user");
 const loginSection = document.getElementById("login");
+const loginBg = document.querySelector(".login_bg");
 
 userDiv.addEventListener("click", () => {
     loginSection.style.display = "flex";
+    loginBg.style.display = "block";
 });
 
-const closeBtn = document.querySelector(".close_btn");
+const closeBtn = document.querySelector(".close_btn img");
 closeBtn.addEventListener("click", () => {
     loginSection.style.display = "none";
+    loginBg.style.display = "none";
 });
 
-window.onload = init;
+loginBg.addEventListener("click", () => {
+    loginSection.style.display = "none";
+    loginBg.style.display = "none";
+});
+
